09989272ffa60a1feb09c729fc9a003a253358f9
Santosh Mane <santoshmane619@gmail.com>
2021-09-28 23:47:53 +0530

    added Global style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <h1>hello</h1>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/style/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *, *::before, *::after{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: none;
+        text-decoration: none;
+    }
+
+`;
+
+export default GlobalStyle;
