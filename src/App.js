27fe48892fbfe5e9a1c4bd69df7b4d582117af37
Sushanth <getsushanthps@gmail.com>
2021-09-30 20:01:07 +0530

    done with chatbot and error page

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import { Navbar } from "./components/navbar/Navbar";
 import { Footer } from "./components/Footer";
 import { ImageSlider } from "./components/ImageSlider";
 import { Login } from "./components/Login";
-
 import { Video } from "./components/Video";
-
 import { Register } from "./components/Register";
-
+import { Error } from "./components/Error";
+import { Chatbot } from "./components/Chatbot";
 
 function App() {
     return (
         <div className="App">
             <Navbar />
-            <Login />
-            <Video />
+            <Error />
+            {/* <Login />
+            <Video /> */}
             {/* <ImageSlider />
             <ProductCard /> */}
             <Footer />
