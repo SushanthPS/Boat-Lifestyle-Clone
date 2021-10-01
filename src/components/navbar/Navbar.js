7b2c1d5f4bb880f1ecb26b8bfae17eb9633c981b
Santosh Mane <santoshmane619@gmail.com>
2021-10-01 23:28:11 +0530

    Added route to champions page form nav

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -165,7 +165,11 @@ export const Navbar = () => {
                         <Collection className="navbar-collection" />
                     </span>
                     <span>Pro It Up</span>
-                    <span>Sound of Champions</span>
+                    <span>
+                        <Link to="/pages/sound-of-champions">
+                            Sound of Champions
+                        </Link>{" "}
+                    </span>
                     <span>
                         More <img src={downArrowIcon} alt="arrow-Icon" />
                     </span>
