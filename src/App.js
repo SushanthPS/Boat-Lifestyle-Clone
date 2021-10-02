a5fc0b7bd72941dcc1aed26b26038d8b2ec912c9
Sushanth <getsushanthps@gmail.com>
2021-10-02 18:11:20 +0530

    adding json data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Navbar } from "./components/navbar/Navbar";
 import { Footer } from "./components/Footer";
 import { Routes } from "./routes/Routes";
-
+import CartPage from "./components/CartPage/CartPage";
+import ProductPage from "./components/ProductPage/ProductPage";
 
 function App() {
     return (
         <div className="App">
             <Navbar />
-            <Routes />
+            {/* <ProductPage /> */}
+            <CartPage />
+            {/* <Routes /> */}
             <Footer />
         </div>
     );
