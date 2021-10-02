295d1b1f7807a1ba58aea3e24df93c794978da4f
Sushanth <getsushanthps@gmail.com>
2021-10-02 19:07:51 +0530

    remove footer from app.js

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,28 +4,34 @@ import { HomePage } from "../components/homepage/HomePage";
 import { Champions } from "../components/Champions";
 import { Login } from "../components/Login";
 import { Register } from "../components/Register";
+import { Footer } from "../components/Footer";
 
 export function Routes() {
     return (
         <Switch>
             <Route path="/" exact>
                 <HomePage />
+                <Footer />
             </Route>
 
             <Route path="/pages/sound-of-champions">
                 <Champions />
+                <Footer />
             </Route>
 
             <Route path="/account/login">
                 <Login />
+                <Footer />
             </Route>
 
             <Route path="/account/register">
                 <Register />
+                <Footer />
             </Route>
 
             <Route>
                 <Error />
+                <Footer />
             </Route>
         </Switch>
     );
