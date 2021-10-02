f79ba854739a967c8b24f7c5fb2e3ec514b9a913
Santosh Mane <santoshmane619@gmail.com>
2021-10-03 04:37:57 +0530

    completes payment page

--- src/payment/Payment.js
+++ src/payment/Payment.js
@@ -3,11 +3,16 @@ import { Link } from "react-router-dom";
 import leftArrow from "../assets/left-arrow.svg";
 import paytmLogo from "../assets/paytm-logo.svg";
 import qrCode from "../assets/qr-code.svg";
+import { useState } from "react";
 
 const PaymentContainer = styled.div`
     width: 100%;
     height: 1270px;
     background: var(--ligth-white);
+
+    a {
+        color: inherit !important;
+    }
 
     & > div {
         width: 1240px;
@@ -137,11 +142,93 @@ const PaymentContainer = styled.div`
                     margin-left: 14px;
                 }
             }
+            .dis1 {
+                font-size: 20px;
+                font-style: normal;
+                font-weight: 300;
+                line-height: 24px;
+                margin-left: 45px;
+                margin-top: 8px;
+            }
+            .moblile-dis {
+                font-size: 14px;
+                font-style: normal;
+                font-weight: 300;
+                line-height: 17px;
+                margin-left: 45px;
+                margin-top: 8px;
+            }
+            .number-input {
+                width: 1091px;
+                height: 72px;
+                border-radius: 8px;
+                display: flex;
+                align-items: center;
+                margin: 22px auto auto auto;
+                margin-bottom: 60px;
+
+                .input1 {
+                    padding-left: 17px;
+                    width: 755px;
+                    height: 100%;
+                    background: var(--white);
+                    outline: none;
+                    border: none;
+                    border-top-left-radius: 8px;
+                    border-bottom-left-radius: 8px;
+                }
+                .proceed {
+                    width: 336px;
+                    height: 100%;
+                    background: #00baf2;
+                    border-radius: 8px;
+                    font-size: 20px;
+                    font-style: normal;
+                    font-weight: 500;
+                    line-height: 24px;
+                    color: var(--white);
+                    display: grid;
+                    align-items: center;
+                    justify-content: center;
+                    cursor: pointer;
+                }
+            }
+            .payment-methods {
+                font-size: 20px;
+                font-style: normal;
+                font-weight: 300;
+                line-height: 24px;
+                display: flex;
+                align-items: center;
+                margin-top: 25px;
+                margin-left: 35px;
+
+                > div {
+                    display: inline;
+                    width: 25px;
+                    height: 25px;
+                    border-radius: 50%;
+                    border: 1px solid #000;
+                    margin-right: 19px;
+                }
+            }
+            .secure {
+                font-size: 12px;
+                font-style: normal;
+                font-weight: 300;
+                line-height: 15px;
+                margin-top: 50px;
+                margin-left: 40px;
+            }
         }
     }
 `;
 
 export const Payment = () => {
+    const [flag, setFlag] = useState(true);
+    const handleFlag = () => {
+        setFlag(false);
+    };
     return (
         <PaymentContainer>
             <div>
@@ -180,6 +267,60 @@ export const Payment = () => {
                             src={paytmLogo}
                             alt={paytmLogo}
                         />
+                    </div>
+                    <p className="dis1">
+                        Pay easily using your saved payment method
+                    </p>
+                    <p className="moblile-dis">
+                        Mobile number registered with paytm
+                    </p>
+                    {flag ? (
+                        <>
+                            <div className="number-input">
+                                <input
+                                    className="input1"
+                                    type="text"
+                                    placeholder="+ 91 1234567890"
+                                    key="input1"
+                                />
+                                <div onClick={handleFlag} className="proceed">
+                                    Proceed
+                                </div>
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <div className="number-input">
+                                <input
+                                    className="input1"
+                                    type="text"
+                                    placeholder="Write Your OTP"
+                                    key="input2"
+                                />
+                                <div className="proceed">
+                                    <Link to="/successfull">Verify</Link>
+                                </div>
+                            </div>
+                        </>
+                    )}
+                    <div className="payment-methods">
+                        <div className="circle"></div>Debit Card
+                    </div>
+                    <div className="payment-methods">
+                        <div className="circle"></div>Credit Card
+                    </div>
+                    <div className="payment-methods">
+                        <div className="circle"></div>Net Banking
+                    </div>
+                    <hr
+                        style={{
+                            marginTop: "35px",
+                            width: 1200,
+                            marginTop: 35,
+                        }}
+                    />
+                    <div className="secure">
+                        100% Secure Payments Powered by Paytm
                     </div>
                 </section>
             </div>
