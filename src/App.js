2ad902b369cd3d3a467c18131359971c7d63ee06
Sushanth <getsushanthps@gmail.com>
2021-10-03 02:47:02 +0530

    readded champions.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     return (
         <div className="App">
             <Navbar />
-            <ProductPage />
+            {/* <ProductPage /> */}
             {/* <CartPage /> */}
-            {/* <Routes /> */}
+            <Routes />
         </div>
     );
 }
