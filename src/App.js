d70d541cc7d8ff1a952570d3fc14157ef6212c92
Sushanth <getsushanthps@gmail.com>
2021-10-03 14:34:23 +0530

    done with collections and dynamic product page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Navbar } from "./components/navbar/Navbar";
 import { Routes } from "./routes/Routes";
+import ProductPage from "./components/ProductPage/ProductPage";
+import { ProductCard } from "./components/ProductCard";
 
 function App() {
     return (
